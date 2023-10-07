a7686a35b2103db330a158cbf1115b96814fb5cb
dfloresca <dfloresca@gmail.com>
2023-10-07 12:25:38 -0700

    chore: added randomized respawn of monsters after grabbing the flag and dying added directions for playing the game and moved outside of the game div

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ let finishFlag;
 window.addEventListener('DOMContentLoaded', function () {
     //load the hero and monster on screen
     hero = new Climber(heroRandomX, game.height - 70, heroImage, 64, 64);
-    monster = new Enemy(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), abomasnowImage, 96, 96);
-    monster2 = new Enemy(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), glalieImage, 96, 96);
-    monster3 = new Enemy(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), articuno, 96, 96)
+    monster = new Enemy(Math.floor(Math.random() * game.width -96), Math.floor(Math.random() * game.height -96), abomasnowImage, 96, 96);
+    monster2 = new Enemy(Math.floor(Math.random() * game.width -96), Math.floor(Math.random() * game.height -96), glalieImage, 96, 96);
+    monster3 = new Enemy(Math.floor(Math.random() * game.width -96), Math.floor(Math.random() * game.height -96), articuno, 96, 96)
     checkPointFlag = new Flag(game.width - 100, game.height * .35, flagImage, 25, 25);
 
     let runGame = this.setInterval(gameLoop, 60);
@@ -176,9 +176,9 @@ function respawnHero() {
         monster.alive = false;
         monster2.alive = false;
         monster3.alive = false;
-        monster = new Enemy(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), abomasnowImage, 96, 96);
-        monster2 = new Enemy(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), glalieImage, 96, 96);
-        monster3 = new Enemy(Math.floor(Math.random() * game.width), Math.floor(Math.random() * game.height), articuno, 96, 96)
+        monster = new Enemy(Math.floor(Math.random() * game.width -96), Math.floor(Math.random() * game.height -96), abomasnowImage, 96, 96);
+        monster2 = new Enemy(Math.floor(Math.random() * game.width -96), Math.floor(Math.random() * game.height -96), glalieImage, 96, 96);
+        monster3 = new Enemy(Math.floor(Math.random() * game.width -96), Math.floor(Math.random() * game.height -96), articuno, 96, 96)
         return true;
     }
 }
